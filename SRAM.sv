2ed9304c384047743123fc72bd438e9794128eb9
Tapti Palit <tapti.palit@stonybrook.edu>
2014-04-09 14:32:14 -0400

    Somewhat fixed isReadValid. Still need to think about the delayCounter=delay assignment, which might end up decrementing delay everytime delayCounter is decremented.

--- SRAM.sv
+++ SRAM.sv
@@ -32,6 +32,8 @@ module SRAM(input[width-1:0] writeData, output[width-1:0] readData, inout isRead
 
    logic [width-1:0] readpipe[delay-1];
 
+   int 		     delayCounter = delay;
+   
    initial begin
       $display("Initializing %0dKB (%0dx%0d) memory, delay = %0d", (width+7)/8*(1<<logDepth)/1024, width, (1<<logDepth), delay);
       assert(ports == 1) else $fatal("multi-ported SRAM not supported");
@@ -42,7 +44,8 @@ module SRAM(input[width-1:0] writeData, output[width-1:0] readData, inout isRead
 	 readpipe[0] <= mem[readAddr];
 	 for(int i=1; i<delay; i=i+1) readpipe[i] <= readpipe[i-1];
 	 readData <= readpipe[delay-1];
-	 if ((delay-1)==0) begin
+	 delayCounter <= delayCounter-1;
+	 if (delayCounter==0) begin
 	    isReadValid = 1;
 	 end
       end else begin
