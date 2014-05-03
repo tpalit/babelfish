2acdcfafafd5c6363d639f6c449273a2a62bb808
Tapti Palit <tapti.palit@stonybrook.edu>
2014-05-03 18:38:41 -0400

    Changed jump.s file. Jumps seem to work.

--- misc/jumps.s
+++ misc/jumps.s
@@ -2,6 +2,7 @@
         .text
 
 _start:
+	/*
 	movq	$0x10, %rax
 	movq	$0x10, %rbx
 	addq	%rax, %rbx
@@ -10,3 +11,22 @@ _start:
 	movq	$0x40, %rcx
 .here:	addq	$0x30, %rdx
         retq
+	*/
+	movq	$0x10, %rax
+	movq	$0x10, %rbx
+	addq	%rax, %rbx
+	subq	$0x2, %rbx
+	jz 	.here
+	jz	.there
+.there: movq $0x11, %rcx
+
+.here:	movq $0x54, %rdx
+	addq %rdx, %rax
+	jz .everywhere
+	movq $0x06, %rax
+	movq $0x07, %rbx
+
+.everywhere:
+	movq $0x11, %rbx
+	retq
+	
